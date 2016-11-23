9d165f88b12180edf52d91aa944b044546112db3
sarika <sarika.ranga@gmail.com>
2016-11-23 15:12:49 -0800

    changes in css javascript less html

---
+++ javascript/11_22_2016/sarika/script.js
@@ -0,0 +1,28 @@
+// 1. NUMBER primitive type
+var a = 20;
+var b = 56.66;
+
+
+// 2. STRING primitive type
+var firstName = 'sarika';
+var lastName = 'ranga';
+
+
+// 3. BOOLEAN primitive type
+var loggedOut = false;
+var loggedIn = true;
+
+
+// 4. null primitive type
+var y = null;
+
+
+// 5. undefined primitive type
+var x = undefined;
+
+console.log(b);
+console.log(lastName);
+console.log(loggedOut);
+console.log(y);
+console.log(x);
+
